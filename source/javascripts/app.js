63d0b2209ba21de5f3d09cbd4c71bc2d9b97430b
ichabodcole <ichabodcole@gmail.com>
2013-04-24 11:46:12 -0700

    Add json data file, Put in skeleton code for backbone app.

---
+++ source/javascripts/app.js
@@ -0,0 +1,17 @@
+$(function(){
+  var ItemModel = Backbone.Model.extend({
+
+  });
+
+  var ItemCollection = Backbone.Collection.extend({
+
+  });
+
+  var ItemView = Backbone.View.extend({
+
+  });
+
+  var ItemList = Backbone.View.extend({
+
+  });
+});
